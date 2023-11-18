b9ceebf1fc91e030ed129fe8fc7eef63b919d84c
Jayson <jaysonchenn17@gmail.com>
2023-11-19 00:26:39 +0630

    setup raw addons index

--- src/pages/backoffice/addons/index.tsx
+++ src/pages/backoffice/addons/index.tsx
@@ -1,8 +1,37 @@
 import Layout from "@/components/Layout";
+import { useAppSelector } from "@/store/hooks";
+import { appData } from "@/store/slices/appSlice";
+import { getAddonsByLocationId, getSelectedLocationId } from "@/utils/client";
 import { Box } from "@mui/material";
+import { useState } from "react";
 
 const Addons = () => {
-  return <p>Addons</p>;
+  const {
+    isLoading,
+    menusMenuCategoriesLocations,
+    menusAddonCategories,
+    addons,
+  } = useAppSelector(appData);
+  const [open, setOpen] = useState(false);
+  const selectedLocationId = getSelectedLocationId() as string;
+  const validAddons = getAddonsByLocationId(
+    selectedLocationId,
+    menusMenuCategoriesLocations,
+    menusAddonCategories,
+    addons
+  );
+  return (
+    <div className="col-span-5">
+      <div className="flex">
+        {validAddons.map((item) => (
+          <div className="flex flex-col m-3 items-center" key={item.id}>
+            <span>{item.name}</span>
+            <span>{item.price}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Addons;
--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,6 +1,29 @@
+import {
+  Addons,
+  MenusAddonCategories,
+  MenusMenuCategoriesLocations,
+} from "@prisma/client";
+
 export const getSelectedLocationId = () => {
   if (typeof window !== "undefined") {
     return localStorage.getItem("selectedLocationId");
   }
   return "";
+};
+
+export const getAddonsByLocationId = (
+  selectedLocationId: string,
+  menusMenuCategoriesLocations: MenusMenuCategoriesLocations[],
+  menusAddonCategories: MenusAddonCategories[],
+  addons: Addons[]
+) => {
+  const validMenuIds = menusMenuCategoriesLocations
+    .filter((item) => item.locationId === Number(selectedLocationId))
+    .map((item) => item.menuId);
+  const validAddonCategoryIds = menusAddonCategories
+    .filter((item) => validMenuIds.includes(item.menuId as number))
+    .map((item) => item.addonCategoryId);
+  return addons.filter((item) =>
+    validAddonCategoryIds.includes(item.addonCategoryId as number)
+  );
 };
