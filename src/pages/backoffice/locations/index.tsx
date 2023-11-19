1f423fd457b7e2b231befca08da567f5b6e22dd5
Jayson <jaysonchenn17@gmail.com>
2023-11-19 11:38:45 +0630

    setup raw locations index

--- src/pages/backoffice/locations/index.tsx
+++ src/pages/backoffice/locations/index.tsx
@@ -1,8 +1,21 @@
 import Layout from "@/components/Layout";
+import { useAppSelector } from "@/store/hooks";
+import { appData } from "@/store/slices/appSlice";
 import { Box } from "@mui/material";
 
 const Locations = () => {
-  return <p>Locations</p>;
+  const { locations, isLoading } = useAppSelector(appData);
+  return (
+    <div className="col-span-5">
+      <div>
+        {locations.map((item) => (
+          <div key={item.id}>
+            <span>{item.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Locations;
