c92ad1c30aa9fe9981f5be0618e7a51751abdd48
Jayson <jaysonchenn17@gmail.com>
2023-11-19 11:48:56 +0630

    setup raw setting index

--- src/pages/backoffice/settings/index.tsx
+++ src/pages/backoffice/settings/index.tsx
@@ -1,8 +1,20 @@
 import Layout from "@/components/Layout";
+import { useAppSelector } from "@/store/hooks";
+import { appData } from "@/store/slices/appSlice";
+import { getSelectedLocationId } from "@/utils/client";
 import { Box } from "@mui/material";
 
 const Settings = () => {
-  return <p>Settings</p>;
+  const { locations, isLoading } = useAppSelector(appData);
+  const locationId = getSelectedLocationId() as string;
+  const validLocation = locations.find(
+    (item) => item.id === Number(locationId)
+  );
+  return (
+    <div>
+      <div>{validLocation?.name}</div>
+    </div>
+  );
 };
 
 export default Settings;
