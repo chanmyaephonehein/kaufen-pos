670af58f9197e62087536187912fc1159b450cba
Jayson <jaysonchenn17@gmail.com>
2023-11-19 11:33:48 +0630

    setup raw tables index

--- src/pages/backoffice/tables/index.tsx
+++ src/pages/backoffice/tables/index.tsx
@@ -1,8 +1,29 @@
 import Layout from "@/components/Layout";
+import { useAppSelector } from "@/store/hooks";
+import { appData } from "@/store/slices/appSlice";
+import { getSelectedLocationId } from "@/utils/client";
 import { Box } from "@mui/material";
+import { useState } from "react";
 
 const Tables = () => {
-  return <p>Tables</p>;
+  const { tables, isLoading } = useAppSelector(appData);
+  const [open, setOpen] = useState(false);
+  const selectedLocationId = getSelectedLocationId() as string;
+
+  const validTables = tables.filter(
+    (item) => item.locationId === Number(selectedLocationId)
+  );
+  return (
+    <div className="col-span-5">
+      <div className="flex">
+        {validTables.map((item) => (
+          <div key={item.id} className="m-3">
+            <span>{item.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Tables;
