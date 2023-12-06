ca39a65e8f3c412b6b5a915960853af18709712e
Jayson <jaysonchenn17@gmail.com>
2023-12-06 20:12:27 +0630

    Order Layout API Fetch

--- src/components/OrderLayout.tsx
+++ src/components/OrderLayout.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { useAppDispatch } from "@/store/hooks";
+import { fetchAppData } from "@/store/slices/appSlice";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 interface Props {
   children: string | JSX.Element | JSX.Element[];
 }
 
 const OrderLayout = ({ children }: Props) => {
+  const { query, isReady, ...router } = useRouter();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (isReady) {
+      dispatch(fetchAppData({ locationId: query.locationId as string }));
+    }
+  }, [query, isReady, dispatch, fetchAppData]);
+
   return (
     <div>
       <div>Lee lh </div>
