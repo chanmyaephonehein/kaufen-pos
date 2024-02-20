4d82de5e308ea8fb6627a62aca5055ccfbffdce4
Jayson <jaysonchenn17@gmail.com>
2024-02-20 10:30:44 +0630

    .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,27 @@
 interface Config {
   apiBaseUrl: string;
-  orderAppUrl: string;
   googleClientId: string;
   googleClientSecret: string;
-  nextAuthSecret: string;
+  spaceEndpoint: string;
+  spaceAccessKeyId: string;
+  spaceSecretAccessKey: string;
+  orderAppUrl: string;
+  environment: string;
 }
 
 export const config: Config = {
   apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || "",
-  orderAppUrl: process.env.NEXT_PUBLIC_ORDER_APP_URL || "",
   googleClientId: process.env.GOOGLE_CLIENT_ID || "",
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-  nextAuthSecret: process.env.NEXT_AUTH_SECRET || "s",
+  spaceAccessKeyId: process.env.SPACE_ACCESS_KEY_ID || "",
+  spaceSecretAccessKey: process.env.SPACE_SECRET_ACCESS_KEY || "",
+  spaceEndpoint: process.env.SPACE_ENDPOINT || "",
+  orderAppUrl: process.env.NEXT_PUBLIC_ORDER_APP_URL || "",
+  environment: process.env.NEXT_PUBLIC_ENVIRONMENT || "",
+};
+
+export const getEnv = () => {
+  if (config.environment === "production") return null;
+  if (config.environment === "development") return "Development";
+  if (config.environment === "staging") return "Staging";
 };
